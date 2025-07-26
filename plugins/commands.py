4e27b4a8a28d23644b8957885abb00ce97ab03da
Ak all bots Control <akallbotscontrol@gmail.com>
2025-07-26 15:49:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,10 +16,7 @@ from os import environ, execle, system
 
 START_TIME = time.time()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Main buttons layout
 main_buttons = [[
     InlineKeyboardButton('❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️', url='https://t.me/kingvj01')
 ],[
@@ -34,25 +31,26 @@ main_buttons = [[
     InlineKeyboardButton('⚙ sᴇᴛᴛɪɴɢs', callback_data='settings#main')
 ]]
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Updated /start with photo
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     user = message.from_user
+
+    # User को DB में save करो अगर नया है
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
+
     reply_markup = InlineKeyboardMarkup(main_buttons)
-    await client.send_message(
+
+    # Photo के साथ caption भेजेंगे
+    await client.send_photo(
         chat_id=message.chat.id,
-        reply_markup=reply_markup,
-        text=Script.START_TXT.format(message.from_user.first_name))
+        photo="https://i.postimg.cc/yx5X5Dxp/file-0000000001886230b82df936231bfb50.png",
+        caption=Script.START_TXT.format(message.from_user.first_name),
+        reply_markup=reply_markup
+    )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Restart command
 @Client.on_message(filters.private & filters.command(['restart']) & filters.user(Config.BOT_OWNER))
 async def restart(client, message):
     msg = await message.reply_text(text="<i>Trying to restarting.....</i>")
@@ -61,10 +59,7 @@ async def restart(client, message):
     system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
     execle(sys.executable, sys.executable, "main.py", environ)
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Help callback
 @Client.on_callback_query(filters.regex(r'^help'))
 async def helpcb(bot, query):
     buttons = [[
@@ -78,10 +73,7 @@ async def helpcb(bot, query):
     reply_markup = InlineKeyboardMarkup(buttons)
     await query.message.edit_text(text=Script.HELP_TXT, reply_markup=reply_markup)
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ How to use callback
 @Client.on_callback_query(filters.regex(r'^how_to_use'))
 async def how_to_use(bot, query):
     buttons = [[InlineKeyboardButton('• back', callback_data='help')]]
@@ -92,10 +84,7 @@ async def how_to_use(bot, query):
         disable_web_page_preview=True
     )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Back callback
 @Client.on_callback_query(filters.regex(r'^back'))
 async def back(bot, query):
     reply_markup = InlineKeyboardMarkup(main_buttons)
@@ -103,10 +92,7 @@ async def back(bot, query):
        reply_markup=reply_markup,
        text=Script.START_TXT.format(query.from_user.first_name))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ About callback
 @Client.on_callback_query(filters.regex(r'^about'))
 async def about(bot, query):
     buttons = [[
@@ -120,10 +106,7 @@ async def about(bot, query):
         disable_web_page_preview=True
     )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Status callback
 @Client.on_callback_query(filters.regex(r'^status'))
 async def status(bot, query):
     users_count, bots_count = await db.total_users_bots_count()
@@ -140,10 +123,7 @@ async def status(bot, query):
         disable_web_page_preview=True,
     )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ System Stats callback
 @Client.on_callback_query(filters.regex(r'^systm_sts'))
 async def sys_status(bot, query):
     buttons = [[InlineKeyboardButton('• back', callback_data='help')]]
@@ -171,25 +151,16 @@ async def sys_status(bot, query):
         disable_web_page_preview=True,
     )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# ✅ Bot uptime function
 async def get_bot_uptime(start_time):
-    # Calculate the uptime in seconds
     uptime_seconds = int(time.time() - start_time)
     uptime_minutes = uptime_seconds // 60
     uptime_hours = uptime_minutes // 60
     uptime_days = uptime_hours // 24
-    uptime_weeks = uptime_days // 7
     uptime_string = ""
     if uptime_hours != 0:
         uptime_string += f" {uptime_hours % 24}H"
     if uptime_minutes != 0:
         uptime_string += f" {uptime_minutes % 60}M"
     uptime_string += f" {uptime_seconds % 60} Sec"
-    return uptime_string   
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    return uptime_string
