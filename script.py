164ddf069a571931c4458186936d34dcbaebf2d3
gajendrakaiwartya <gajendrakaiwartya29@gmail.com>
2025-08-01 15:52:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -38,9 +38,9 @@ class  Script(object):
   ABOUT_TXT = """<b>
 ╔════❰ ғᴏʀᴡᴀʀᴅ ʙᴏᴛ ❱═❍⊱❁۪۪
 ║╭━━━━━━━━━━━━━━━➣
-║┣⪼📃ʙᴏᴛ : [Fᴏʀᴡᴀᴅ Bᴏᴛ](https://t.me/VJForwardBot)
-║┣⪼👦Cʀᴇᴀᴛᴏʀ : [Kɪɴɢ VJ 👑](https://t.me/kingvj01)
-║┣⪼🤖Uᴘᴅᴀᴛᴇ : [VJ Bᴏᴛᴢ](https://t.me/vj_botz)
+║┣⪼📃ʙᴏᴛ : [Fᴏʀᴡᴀᴅ Bᴏᴛ](https://t.me/AK_BOTZ_FORWARD_BOT)
+║┣⪼👦Cʀᴇᴀᴛᴏʀ : [ABHAY KAIWARTYA](https://t.me/MR_ABHAY_K)
+║┣⪼🤖Uᴘᴅᴀᴛᴇ : [AK BOTZ UPDATE](https://t.me/AK_BOTZ_UPDATE)
 ║┣⪼📡Hᴏsᴛᴇᴅ ᴏɴ : Sᴜᴘᴇʀ Fᴀsᴛ
 ║┣⪼🗣️Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ3
 ║┣⪼📚Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ Gᴀᴛʜᴇʀ 2.11.0 
